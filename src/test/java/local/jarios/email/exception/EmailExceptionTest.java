5ce2b841550f6e9c33d5956483bc174ecfbd28c1
contratacionmalaga <incidenciascontratacion@malaga.es>
2025-06-22 17:45:37 +0200

    Actualización de la API

--- src/test/java/local/jarios/email/exception/EmailExceptionTest.java
+++ src/test/java/local/jarios/email/exception/EmailExceptionTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class EmailServiceExceptionTest {
+class EmailExceptionTest {
 
     @Test
     void testConstructorWithMessage() {
